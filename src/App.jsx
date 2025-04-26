c29a3e441c2a3305e0793eb6c79fa77862b6bb62
itlord23 <nguyenthihonghanh5098@gmail.com>
2025-04-26 10:11:22 +0700

    dựng khung phần Exercise nhưng chưa xử lý gì trong phần làm bài tập

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./pages/Home"
 import Dictionary from "./pages/dictionary/Dictionary"
 import Flashcard from "./pages/flashcard/Flashcard"
 import Exercise from "./pages/exercise/Excercise"
+import { ExerciseProvider } from "./context/ExerciseContext"; // add
+import ExerciseResult from "./pages/exercise/ExerciseResult";
 import Mindmap from "./pages/mindmap/Mindmap"
 import { AppContextProvider } from "./context"
 
@@ -15,7 +17,19 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/dictionary" element={<Dictionary />} />
                     <Route path="/flashcard" element={<Flashcard />} />
-                    <Route path="/exercise" element={<Exercise />} />
+
+                    <Route
+                        path="/exercise/*"
+                        element={
+                            <ExerciseProvider>
+                                <Routes>
+                                    <Route path="" element={<Exercise />} />
+                                    <Route path="result" element={<ExerciseResult />} />
+                                </Routes>
+                            </ExerciseProvider>
+                        }
+                    />
+
                     <Route path="/mindmap" element={<Mindmap />} />
                 </Routes>
             </MainLayout>
